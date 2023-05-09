1640ec21f55af86a2918daa79fd391b4edd5e440
Nodir0624 <nodir1993@gmail.com>
2023-05-09 13:50:25 -0400

    update second line

--- scenario1.py
+++ scenario1.py
@@ -1,3 +1,3 @@
 # This is the test file for git version control system
 print("scenario 1: Login to website.")
-print("Adding line from Github")
+print("Adding line from Github and updating from pycharm")
